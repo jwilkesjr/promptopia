561c2e89c239aa24064868a9b96ee0e9d7f3e2df
jwilkesjr <jrjaygaming@gmail.com>
2023-06-27 15:27:44 -0700

    most update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const[toggleDropdown, setToggleDropdown] = useState(false);
@@ -37,7 +37,7 @@ const Nav = () => {
   </Link>
   {/* Desktop Navigation */}
   <div className="sm:flex hidden">
-    {isUserLoggedIn ? (
+    {session?.user ? (
       <div className="flex gap-3 md:gap-5">
         <Link 
           href="/create-prompt"
@@ -83,7 +83,7 @@ const Nav = () => {
 
   {/* Mobile Navigation */}
   <div className="sm:hidden flex relative">
-    {isUserLoggedIn ? (
+    {session?.user ? (
       <div className="flex">
         <Image
           src="/assets/images/logo.svg"
